2d0286aeb8f2787753d1586994317c4446bbdc76
clayhindman01 <clayhindman01@gmail.com>
2022-07-18 23:18:22 -0400

    added login screen and functionality

---
+++ database/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAVWAPNk-ZT2iBspROdcFMTMbyJOVoC3As",
+    authDomain: "mosaic-76e3c.firebaseapp.com",
+    projectId: "mosaic-76e3c",
+    storageBucket: "mosaic-76e3c.appspot.com",
+    messagingSenderId: "471984825969",
+    appId: "1:471984825969:web:170877dfb0fd7d2cbf5dd0",
+    measurementId: "G-PL0GTDTZPS"
+  };
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
